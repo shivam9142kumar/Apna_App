86f6bd9be5b21e27932c0dbba3f7e46deaeb1642
Yash Bhardwaj <149149660+yash9939@users.noreply.github.com>
2024-06-19 19:26:55 +0530

    Backend

--- Apna App BackEnd.py
+++ Apna App BackEnd.py
@@ -1,16 +1,16 @@
-# Importing the sqlinte3 Module for the BackEnd
+#Importing the sqlinte3 Module for the BackEnd
 import sqlite3
 
-# Defining the function for adding the Details of User in the Database
-def addData(Name, Email_id, Skills, Phone_no, Degree):
+#Defining the function for adding the Details of User in the Database
+def addData(Name, Email_id, Skills, Phone_no, Degree, Password):
     con = sqlite3.connect("apna.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO User VALUES (NULL, ?, ?, ?, ?, ?)", Name, Email_id, Skills, Phone_no, Degree)
+    cur.execute("INSERT INTO User VALUES (NULL, ?, ?, ?, ?, ?, ?)", Name, Email_id, Skills, Phone_no, Degree, Password)
     con.commit()
     con.close()
 
 
-# Defining the function for Viewing the Details of User in the Database
+#Defining the function for Viewing the Details of User in the Database
 def viewData():
     con = sqlite3.connect("apna.db")
     cur = con.cursor()
@@ -20,10 +20,10 @@ def viewData():
     return user_val
 
 
-# Defining the function for deleting the Details of User in the Database
+#Defining the function for deleting the Details of User in the Database
 def DeleteData(uid):
     con = sqlite3.connect("apna.db")
     cur = con.cursor()
-    cur.execute("DELETE FROM User WHERE uid = ?", (uid, ))
+    cur.execute("DELETE FROM User WHERE uid = ?", (uid ))
     user_val = cur.fetchall()
     con.close()
